80e37c6f170dce1b8280b5296ec9c99931c0f0ef
Tecsup <tecsup@MacBook-Pro-de-Tecsup-16.local>
2026-08-19 15:13:41 -0500

    agregue mi nombre al saludo

--- app/src/main/java/com/leon/labb00/MainActivity.kt
+++ app/src/main/java/com/leon/labb00/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Labb00Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Soy Daniella",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
